fbdaee859afcd15a7024f210a60a665f4b8c25d8
Jos de Jong <wjosdejong@gmail.com>
2023-09-05 23:33:53 +0200

    Add some more benchmarks

--- README.md
+++ README.md
@@ -6,9 +6,12 @@ To run:
 npm install
 npm run benchmark1
 npm run benchmark2
+npm run benchmark3
+npm run benchmark4
+npm run benchmark5
 ```
 
-Output of benchmark1:
+Output of benchmark1 (HPI_master.csv):
 
 ```
 udsv x 9.42 ops/sec ±3.74% (28 runs sampled)
@@ -16,10 +19,34 @@ csv42 x 4.38 ops/sec ±3.70% (16 runs sampled)
 papaparse x 2.33 ops/sec ±2.47% (10 runs sampled)
 ```
 
-Output of benchmark2:
+Output of benchmark2 (large-dataset.csv):
 
 ```
 udsv x 65.61 ops/sec ±0.79% (69 runs sampled)
 csv42 x 44.95 ops/sec ±0.43% (59 runs sampled)
 papaparse x 25.84 ops/sec ±2.39% (47 runs sampled)
+```
+
+Output of benchmark3 (litmus_ints.csv):
+
+```
+udsv x 73.26 ops/sec ±0.91% (75 runs sampled)
+csv42 x 17.21 ops/sec ±4.07% (47 runs sampled)
+papaparse x 13.69 ops/sec ±0.92% (37 runs sampled)
+```
+
+Output of benchmark4 (litmus_quoted.csv):
+
+```
+udsv x 66.30 ops/sec ±3.91% (69 runs sampled)
+csv42 x 11.46 ops/sec ±1.97% (33 runs sampled)
+papaparse x 8.69 ops/sec ±3.52% (26 runs sampled)
+```
+
+Output of benchmark5 (litmus_strings.csv):
+
+```
+udsv x 107 ops/sec ±2.79% (79 runs sampled)
+csv42 x 13.18 ops/sec ±1.91% (37 runs sampled)
+papaparse x 12.53 ops/sec ±2.65% (36 runs sampled)
 ```
---
+++ litmus_gen.cjs
@@ -0,0 +1,143 @@
+const fs = require('fs');
+
+let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ';
+
+function randStr(length) {
+  let s = '';
+
+  for (; s.length < length; s += chars.charAt(Math.random() * 63 | 0));
+
+  return s;
+}
+
+function randInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let rows = 1e4;
+let cols = 20;
+let out = [];
+
+for (let i = 0; i < rows; i++) {
+  out.push(
+    Array.from(
+      { length: cols },
+      () => randStr(randInt(3, 15))
+    ).join(',')
+  );
+}
+
+fs.writeFileSync(__dirname + '/data/litmus_strings.csv', out.join('\n'));
+
+out = [];
+
+for (let i = 0; i < rows; i++) {
+  out.push(
+    Array.from(
+      { length: cols },
+      () => i === 0 ? randStr(randInt(3, 15)) : randInt(-10_000, 10_000)
+    ).join(',')
+  );
+}
+
+fs.writeFileSync(__dirname + '/data/litmus_ints.csv', out.join('\n'));
+
+
+out = [];
+
+for (let i = 0; i < rows; i++) {
+  out.push(
+    Array.from(
+      { length: cols },
+      (v, j) => i === 0 || j % 2 === 0 ? `"${randStr(randInt(3, 15))}"` : `"${randInt(-10_000, 10_000)}"`
+    ).join(',')
+  );
+}
+
+fs.writeFileSync(__dirname + '/data/litmus_quoted.csv', out.join('\n'));
+
+
+/*
+// https://github.com/josdejong/csv42/tree/main/benchmark
+
+let len = 10_000;
+
+{
+  let testHuge = Array(len);
+
+  testHuge[0] = `_type,name,description,city,street,latitude,longitude,speed,heading,"field with , delimiter","field with "" double quote"`;
+
+  for (let i = 1; i < len; i++) {
+    testHuge[i] = `item,Item ${i},Item ${i} description in text,Rotterdam,Main street,${(Math.random() * 100).toFixed(7)},${(Math.random() * 100).toFixed(7)},5.4,128.3,"value with , delimiter","value with "" double quote"`;
+  }
+
+  fs.writeFileSync('./csv42_flat_10k.csv', testHuge.join("\n"));
+}
+
+{
+  let testHuge = Array(len);
+
+  testHuge[0] = `_type,name,description,location.city,location.street,location.geo[0],location.geo[1],speed,heading,size[0],size[1],size[2],"field with , delimiter","field with "" double quote"`;
+
+  for (let i = 1; i < len; i++) {
+    testHuge[i] = `item,Item ${i},Item ${i} description in text,Rotterdam,Main street,${(Math.random() * 100).toFixed(7)},${(Math.random() * 100).toFixed(7)},5.4,128.3,3.4,5.1,0.9,"value with , delimiter","value with "" double quote"`;
+  }
+
+  fs.writeFileSync('./csv42_nested_10k.csv', testHuge.join("\n"));
+}
+*/
+
+/*
+// https://github.com/LeanyLabs/csv-parsers-benchmarks
+// https://github.com/willfarrell/csv-benchmarks
+
+let files = [
+  {
+    columns: 10,
+    rows: 10_000,
+    cycles: 20,
+  },
+  {
+    columns: 100,
+    rows: 10_000,
+    cycles: 10,
+  },
+  {
+    columns: 10,
+    rows: 100_000,
+    cycles: 10,
+  },
+  {
+    columns: 100,
+    rows: 100_000,
+    cycles: 5,
+  },
+  {
+    columns: 10,
+    rows: 1_000_000,
+    cycles: 5,
+  },
+];
+
+function gen(quotes) {
+  const wrapper = quotes ? '"' : ''
+  const delimiter = quotes ? '","' : ','
+
+  files.forEach(({columns, rows}) => {
+    let buf = '';
+
+    buf += wrapper + Array.from({ length: columns }, (_, x) => `col${x}`).join(delimiter) + wrapper + '\r\n';
+
+    for (let y = 0; y < rows; y++) {
+      buf += wrapper + Array.from({ length: columns }, (_, x) => `${x}x${y}`).join(delimiter) + wrapper + '\r\n';
+    }
+
+    fs.writeFileSync(`./bench/data/${columns}x${rows}_${quotes ? 'quoted' : 'slim'}.csv`, buf);
+  });
+}
+
+gen(true);
+gen(false);
+*/
