e38c7654aa6426168b7e5d41410a075fd2af9f5d
Jos de Jong <wjosdejong@gmail.com>
2023-09-07 20:10:35 +0200

    Print task name

--- benchmark-custom.js
+++ benchmark-custom.js
@@ -15,7 +15,7 @@ function geoMean(arr) {
 
 const sleep = () => new Promise(resolve => setImmediate(resolve));
 
-async function customBench(task) {
+async function customBench(name, task) {
 
   let cycles = 200;
   let durs = [];
@@ -30,9 +30,9 @@ async function customBench(task) {
     durs.push(performance.now() - st);
     rss.push(process.memoryUsage.rss());
   }
-  console.log((1e3 / geoMean(durs)).toFixed(1) + ' ops/s', (Math.max(...rss) / 1024 / 1024).toFixed(1) + ' peak RSS (MiB)');
+  console.log(name, (1e3 / geoMean(durs)).toFixed(1) + ' ops/s', (Math.max(...rss) / 1024 / 1024).toFixed(1) + ' peak RSS (MiB)');
 }
 
-await customBench(() => useUdsvDeep(data))
-await customBench(() => useCsv42(data))
-await customBench(() => usePapaParseDeep(data))
+await customBench('udsv', () => useUdsvDeep(data))
+await customBench('csv42', () => useCsv42(data))
+await customBench('papaparse+flat', () => usePapaParseDeep(data))
